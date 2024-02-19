d897376f7ed29315821cbf1cc4ef11bfc5976231
Son Nguyen Thanh <69808662+bjergsen243@users.noreply.github.com>
2024-02-20 01:34:43 +0700

    feat: complete chapter 6

--- ch06/src/routes/question.rs
+++ ch06/src/routes/question.rs
@@ -1,21 +1,27 @@
 use std::collections::HashMap;
-use warp::http::StatusCode;
 
+use warp::http::StatusCode;
+use tracing::{instrument, info};
+
+use handle_errors::Error;
 use crate::store::Store;
 use crate::types::pagination::extract_pagination;
 use crate::types::question::{Question, QuestionId};
-use handle_errors::Error;
 
+#[instrument]
 pub async fn get_questions(
     params: HashMap<String, String>,
     store: Store,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    info!("querying questions");
     if !params.is_empty() {
         let pagination = extract_pagination(params)?;
+        info!(pagination = true);
         let res: Vec<Question> = store.questions.read().await.values().cloned().collect();
         let res = &res[pagination.start..pagination.end];
         Ok(warp::reply::json(&res))
     } else {
+        info!(pagination = false);
         let res: Vec<Question> = store.questions.read().await.values().cloned().collect();
         Ok(warp::reply::json(&res))
     }
