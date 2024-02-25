2d6dbac309027d6636418a9716de161e5c3e8669
Sotatek-SonNguyen11 <son.nguyen11@sotatek.com>
2024-02-25 17:40:08 +0700

    feat: add unit test for pagination and config and authentication method

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
-use dotenv;
 use std::env;
 
 /// Q&A web service API
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, PartialEq)]
 #[clap(author, version, about, long_about = None)]
 pub struct Config {
     /// Which errors we want to log (info, warn or error)
@@ -25,18 +24,13 @@ pub struct Config {
     #[clap(long, default_value = "5432")]
     pub db_port: u16,
     /// Database name
-    #[clap(long, default_value = "rust_hour")]
+    #[clap(long, default_value = "rustwebdev")]
     pub db_name: String,
 }
 
 impl Config {
     pub fn new() -> Result<Config, handle_errors::Error> {
-        dotenv::dotenv().ok();
         let config = Config::parse();
-
-        if env::var("BAD_WORDS_API_KEY").is_err() {
-            panic!("BadWords API key not set");
-        }
 
         if env::var("PASETO_KEY").is_err() {
             panic!("PASETO_KEY not set");
@@ -65,5 +59,44 @@ impl Config {
                 .map_err(handle_errors::Error::ParseError)?,
             db_name,
         })
+    }
+}
+
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+
+    fn set_env() {
+        env::set_var("PASETO_KEY", "RANDOM WORDS WINTER MACINTOSH PC");
+        env::set_var("POSTGRES_USER", "sotatek");
+        env::set_var("POSTGRES_PASSWORD", "1");
+        env::set_var("POSTGRES_HOST", "localhost");
+        env::set_var("POSTGRES_PORT", "5432");
+        env::set_var("POSTGRES_DB", "rustwebdev");
+    }
+
+    #[test]
+    fn unset_and_set_api_key() {
+        // ENV VARIABLES ARE NOT SET
+        let result = std::panic::catch_unwind(|| Config::new());
+        assert!(result.is_err());
+
+        // NOW WE SET THEM
+        set_env();
+
+        let expected = Config {
+            log_level: "warn".to_string(),
+            port: 8080,
+            db_user: "sotatek".to_string(),
+            db_password: "1".to_string(),
+            db_host: "localhost".to_string(),
+            db_port: 5432,
+            db_name: "rustwebdev".to_string(),
+        };
+
+        let config = Config::new().unwrap();
+
+        assert_eq!(config, expected);
     }
 }
