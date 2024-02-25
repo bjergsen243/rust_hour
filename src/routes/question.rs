5cfa7df8909e0c0943008c8cb38943b51924a8ea
Sotatek-SonNguyen11 <son.nguyen11@sotatek.com>
2024-02-25 16:22:05 +0700

    chore: remove profanity

--- src/routes/question.rs
+++ src/routes/question.rs
@@ -3,7 +3,6 @@ use std::collections::HashMap;
 use tracing::{event, instrument, Level};
 use warp::http::StatusCode;
 
-use crate::profanity::check_profanity;
 use crate::store::Store;
 use crate::types::account::Session;
 use crate::types::pagination::{extract_pagination, Pagination};
@@ -39,26 +38,15 @@ pub async fn update_question(
 ) -> Result<impl warp::Reply, warp::Rejection> {
     let account_id = session.account_id;
     if store.is_question_owner(id, &account_id).await? {
-        let title = check_profanity(question.title);
-        let content = check_profanity(question.content);
-
-        let (title, content) = tokio::join!(title, content);
-
-        if title.is_ok() && content.is_ok() {
-            let question = Question {
-                id: question.id,
-                title: title.unwrap(),
-                content: content.unwrap(),
-                tags: question.tags,
-            };
-            match store.update_question(question, id, account_id).await {
-                Ok(res) => Ok(warp::reply::json(&res)),
-                Err(e) => Err(warp::reject::custom(e)),
-            }
-        } else {
-            Err(warp::reject::custom(
-                title.expect_err("Expected API call to have failed here"),
-            ))
+        let question = Question {
+            id: question.id,
+            title: question.title,
+            content: question.content,
+            tags: question.tags,
+        };
+        match store.update_question(question, id, account_id).await {
+            Ok(res) => Ok(warp::reply::json(&res)),
+            Err(e) => Err(warp::reject::custom(e)),
         }
     } else {
         Err(warp::reject::custom(handle_errors::Error::Unauthorized))
@@ -90,19 +78,10 @@ pub async fn add_question(
     new_question: NewQuestion,
 ) -> Result<impl warp::Reply, warp::Rejection> {
     let account_id = session.account_id;
-    let title = match check_profanity(new_question.title).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
-
-    let content = match check_profanity(new_question.content).await {
-        Ok(res) => res,
-        Err(e) => return Err(warp::reject::custom(e)),
-    };
 
     let question = NewQuestion {
-        title,
-        content,
+        title: new_question.title,
+        content: new_question.content,
         tags: new_question.tags,
     };
 
